968ae5b7456976017f7bfafd8d43837f02d03053
SHobhit Srivastava <shobhit.srivastava91@gmail.com>
2015-04-22 22:56:13 +0530

    moviewhereabouts

--- moviewhereabouts/urls.py
+++ moviewhereabouts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'moviewhereabouts.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^moviewhereabouts.herokuapp.com/asteroid/', include('asteroid.urls', namespace="asteroid")),
+    url(r'^moviewhereabouts.herokuapp.com', include('asteroid.urls', namespace="asteroid")),
     url(r'^admin/', include(admin.site.urls)),
 )
