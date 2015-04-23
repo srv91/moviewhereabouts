9c4ad217919811f4e1aa2185caa58802d5586854
SHobhit Srivastava <shobhit.srivastava91@gmail.com>
2015-04-24 00:41:29 +0530

    moviewhereabouts

--- moviewhereabouts/wsgi.py
+++ moviewhereabouts/wsgi.py
@@ -11,7 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviewhereabouts.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
